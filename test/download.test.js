66b5aca8d95346efe6e038ea469d5298f4f37a3f
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2020-10-15 11:41:00 -0300

    fix: font flickers when changing page

--- test/download.test.js
+++ test/download.test.js
@@ -1,6 +1,6 @@
 const { join } = require('path')
 const del = require('del')
-const { setup, loadConfig, get } = require('@nuxtjs/module-test-utils')
+const { setup, loadConfig } = require('@nuxtjs/module-test-utils')
 
 describe('download', () => {
   let nuxt
@@ -14,16 +14,7 @@ describe('download', () => {
     await del(join(nuxt.options.srcDir, nuxt.options.dir.assets))
   })
 
-  test('render', async () => {
-    const html = await get('/')
-    expect(html).not.toContain('<link data-n-head="ssr" rel="dns-prefetch" href="https://fonts.gstatic.com/">')
-    expect(html).not.toContain('<link data-n-head="ssr" rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="true">')
-    expect(html).not.toContain('<link data-n-head="ssr" rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Roboto">')
-    expect(html).not.toContain('<link data-n-head="ssr" vmid="google-fonts" rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto" media="print" onload="this.onload=null;this.removeAttribute')
-    expect(html).not.toContain('<noscript data-n-head="ssr"><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto"></noscript>')
-  })
-
-  test('css inject', () => {
+  test('render', () => {
     expect(nuxt.options.css).toHaveLength(1)
     expect(nuxt.options.css[0]).toContain('fonts.css')
   })
