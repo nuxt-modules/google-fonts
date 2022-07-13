a67752d1afb839eab6123d5d3f8b3c956f054535
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-07-13 13:18:28 -0300

    chore: add playground

---
+++ playground/app.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <div>
+    <h1>Google Fonts</h1>
+    <div style="font-family: 'Roboto';">
+      Roboto
+    </div>
+    <div style="font-family: 'Mulish';">
+      Mulish
+    </div>
+  </div>
+</template>
