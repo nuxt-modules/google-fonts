04f01ac72ab08a68d74ad6373158400d6aae22c5
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-08-14 15:37:59 -0300

    docs: update colors

---
+++ docs/tokens.config.ts
@@ -0,0 +1,18 @@
+import { defineTheme } from 'pinceau'
+
+export default defineTheme({
+  color: {
+    primary: {
+      50: '#eff6ff',
+      100: '#dbeafe',
+      200: '#bfdbfe',
+      300: '#93c5fd',
+      400: '#60a5fa',
+      500: '#1A73E8',
+      600: '#2563eb',
+      700: '#1d4ed8',
+      800: '#1e40af',
+      900: '#1e3a8a'
+    }
+  }
+})
