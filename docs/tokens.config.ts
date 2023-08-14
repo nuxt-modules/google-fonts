2f3b017b2b94d4c41e222b12f8233c081ffe3d83
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-08-14 15:59:29 -0300

    docs: update colors

--- docs/tokens.config.ts
+++ docs/tokens.config.ts
@@ -3,16 +3,16 @@ import { defineTheme } from 'pinceau'
 export default defineTheme({
   color: {
     primary: {
-      50: '#eff6ff',
-      100: '#dbeafe',
-      200: '#bfdbfe',
-      300: '#93c5fd',
-      400: '#60a5fa',
-      500: '#1A73E8',
-      600: '#2563eb',
-      700: '#1d4ed8',
-      800: '#1e40af',
-      900: '#1e3a8a'
+      50: '#eff8ff',
+      100: '#dbeefe',
+      200: '#c0e2fd',
+      300: '#94d1fc',
+      400: '#61b7f9',
+      500: '#3d97f4',
+      600: '#1b73e8',
+      700: '#1f64d6',
+      800: '#1f52ae',
+      900: '#1f4689'
     }
   }
 })
